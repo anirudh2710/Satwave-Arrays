8aba4039932697439d5df56cb222955286d2d040
Anirudh Sowmyanarayanan <anikrish1998@gmail.com>
2026-05-16 10:58:40 -0400

    chore: upgrade Sanity dependencies, configure studio host, and pin sub-dependencies via overrides

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -1,13 +1,14 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
     projectId: 'eikhmyae',
     dataset: 'production'
   },
+  studioHost: 'satwave',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
-  autoUpdates: true,
+  autoUpdates: false,
 })
